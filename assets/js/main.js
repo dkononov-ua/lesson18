984ea2f32d39a9542d1b95269ea64e799637227f
Denys <dkononov.ua@gmail.com>
2022-11-29 12:04:41 +0200

    оформлення

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,78 +1,57 @@
-let $slides = $(".slides__item");
-let $indContainer = $(".indicators");
-let $indItems = $(".indicators__item");
-let currentSlide = 0;
-let carouselInterval = 2000;
-
-const SPACE = " ";
-const LEFT_ARROW = "ArrowLeft";
-const RIGHT_ARROW = "ArrowRight";
-const FA_PAUSE = '<i class="fas fa-pause"></i>';
-const FA_PLAY = '<i class="fas fa-play"></i>';
-
-$indContainer.css("display", "flex");
-$(".controls").css("display", "block");
-
-let gotoNSlide = (n) => {
-  $($slides[currentSlide]).toggleClass("active");
-  $($indItems[currentSlide]).toggleClass("active");
-  currentSlide = (n + $slides.length) % $slides.length;
-  $($slides[currentSlide]).toggleClass("active");
-  $($indItems[currentSlide]).toggleClass("active");
-};
-
-let gotoNextSlide = () => gotoNSlide(currentSlide + 1);
-
-let gotoPrevSlide = () => gotoNSlide(currentSlide - 1);
-
-let playbackStatus = true;
-let $pausePlayBtn = $(".indicators__pause");
-let $nextBtn = $(".controls__next");
-let $prevBtn = $(".controls__prev");
-let slideInterval = setInterval(gotoNextSlide, carouselInterval);
-
-let pauseSlideShow = () => {
-  if (playbackStatus) {
-    $pausePlayBtn.html(FA_PAUSE);
-    playbackStatus = !playbackStatus;
-    clearInterval(slideInterval);
-  }
-};
-
-let playSlideShow = () => {
-  $pausePlayBtn.html(FA_PLAY);
-  playbackStatus = !playbackStatus;
-  slideInterval = setInterval(gotoNextSlide, carouselInterval);
-};
-
-let clickPausePlayBtn = () =>
-  playbackStatus ? pauseSlideShow() : playSlideShow();
-
-let clickNextBtn = () => {
-  pauseSlideShow();
-  gotoNextSlide();
-};
-
-let clickPrevBtn = () => {
-  pauseSlideShow();
-  gotoPrevSlide();
-};
-
-$pausePlayBtn.on("click", clickPausePlayBtn);
-$nextBtn.on("click", clickNextBtn);
-$prevBtn.on("click", clickPrevBtn);
-
+let $slides = $(".slides__item"),
+  $indContainer = $(".indicators"),
+  $indItems = $(".indicators__item"),
+  currentSlide = 0,
+  carouselInterval = 2000;
+const SPACE = " ",
+  LEFT_ARROW = "ArrowLeft",
+  RIGHT_ARROW = "ArrowRight",
+  FA_PAUSE = '<i class="fas fa-play-circle"></i>',
+  FA_PLAY = '<i class="fas fa-pause-circle"></i>';
+$indContainer.css("display", "flex"), $(".controls").css("display", "block");
+let gotoNSlide = (e) => {
+    $($slides[currentSlide]).toggleClass("active"),
+      $($indItems[currentSlide]).toggleClass("active"),
+      (currentSlide = (e + $slides.length) % $slides.length),
+      $($slides[currentSlide]).toggleClass("active"),
+      $($indItems[currentSlide]).toggleClass("active");
+  },
+  gotoNextSlide = () => gotoNSlide(currentSlide + 1),
+  gotoPrevSlide = () => gotoNSlide(currentSlide - 1),
+  playbackStatus = !0,
+  $pausePlayBtn = $(".controls__pause"),
+  $nextBtn = $(".controls__next"),
+  $prevBtn = $(".controls__prev"),
+  slideInterval = setInterval(gotoNextSlide, carouselInterval),
+  pauseSlideShow = () => {
+    playbackStatus &&
+      ($pausePlayBtn.html(FA_PAUSE),
+      (playbackStatus = !playbackStatus),
+      clearInterval(slideInterval));
+  },
+  playSlideShow = () => {
+    $pausePlayBtn.html(FA_PLAY),
+      (playbackStatus = !playbackStatus),
+      (slideInterval = setInterval(gotoNextSlide, carouselInterval));
+  },
+  clickPausePlayBtn = () =>
+    playbackStatus ? pauseSlideShow() : playSlideShow(),
+  clickNextBtn = () => {
+    pauseSlideShow(), gotoNextSlide();
+  },
+  clickPrevBtn = () => {
+    pauseSlideShow(), gotoPrevSlide();
+  };
+$pausePlayBtn.on("click", clickPausePlayBtn),
+  $nextBtn.on("click", clickNextBtn),
+  $prevBtn.on("click", clickPrevBtn);
 let clickIndicatorBtn = (e) => {
-  pauseSlideShow();
-  gotoNSlide(+e.target.getAttribute("data-slide-to"));
+  pauseSlideShow(), gotoNSlide(+e.target.getAttribute("data-slide-to"));
 };
-
 $indContainer.on("click", ".indicators__item", clickIndicatorBtn);
-
 let pressKeyControl = (e) => {
-  if (e.key === LEFT_ARROW) clickPrevBtn();
-  if (e.key === RIGHT_ARROW) clickNextBtn();
-  if (e.key === SPACE) clickPausePlayBtn();
+  e.key === LEFT_ARROW && clickPrevBtn(),
+    e.key === RIGHT_ARROW && clickNextBtn(),
+    " " === e.key && clickPausePlayBtn();
 };
-
 $(document).on("keydown", pressKeyControl);
